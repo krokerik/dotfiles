3568235f1c1659be693632131736aa82a11c658c
Erik Andersson <dv13ean@cs.umu.se>
2016-05-28 22:31:45 +0200

    created install script

--- scripts/music.sh
+++ scripts/music.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-SONG=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk '/xesam:title/{getline; print $NF}' | awk -F'\"' '{print $2}'
-ARTIST=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk '/xesam:artist/{getline; getline; print $NF}' | awk -F'\"' '{print $2}'
-OUTPUT=""
+SONG=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk '/xesam:title/{getline; print}' | cut -d '"' -f2)
+ARTIST=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk '/xesam:artist/{getline; getline; print}' | cut -d '"' -f2)
+STATUS=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' | cut -d '"' -f2)
+
 ICON=""
-
-if [ -n "$SONG" ]; then
-    OUTPUT="$ARTIST - $SONG"
+if [ -n "$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' | cut -d '"' -f2 | grep -E "Playing")" ]; then
+	ICON="25b6"
 else
-    OUTPUT="None"
+	ICON="2161"
 fi
-
-echo "${OUTPUT}"
+if [ -n "$SONG" ]; then
+    echo "\u${ICON} $ARTIST - $SONG"
+else
+    OUTPUT="none"
+fi
 
 exit 0
 
